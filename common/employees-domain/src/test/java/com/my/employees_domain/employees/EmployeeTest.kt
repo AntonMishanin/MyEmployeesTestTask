6af51ba3f137ae9619c3167c463b7acd26881023
Anton Mishanin <antonmishanin@mail.ru>
2022-07-13 17:19:06 +0700

    0008: test correct birthday logic

---
+++ common/employees-domain/src/test/java/com/my/employees_domain/employees/EmployeeTest.kt
@@ -0,0 +1,76 @@
+package com.my.employees_domain.employees
+
+import org.junit.Assert
+import org.junit.Test
+
+/**
+ * @Author: Anton Mishanin
+ * @Date: 7/13/2022
+ */
+internal class EmployeeTest {
+
+    @Test
+    fun `test the birthday is empty should be empty`() {
+        val employee = employee(birthDay = "")
+
+        val actual = employee.copyWithCorrectBirthday().birthday
+        val expected = ""
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `test the year in the start the birthday should be correct`() {
+        val employee = employee(birthDay = "1987-03-23")
+
+        val actual = employee.copyWithCorrectBirthday().birthday
+        val expected = "23.03.1987"
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `test the year in the middle the birthday should be correct`() {
+        val employee = employee(birthDay = "03-1987-23")
+
+        val actual = employee.copyWithCorrectBirthday().birthday
+        val expected = "23.03.1987"
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `test the year in the end the birthday should be correct`() {
+        val employee = employee(birthDay = "03-23-1987")
+
+        val actual = employee.copyWithCorrectBirthday().birthday
+        val expected = "23.03.1987"
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `test the month in the middle the birthday should be correct`() {
+        val employee = employee(birthDay = "23-03-1987")
+
+        val actual = employee.copyWithCorrectBirthday().birthday
+        val expected = "23.03.1987"
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `test the wrong birthday should return himself`() {
+        var actual = employee(birthDay = "2t-03-1987").copyWithCorrectBirthday().birthday
+        var expected = "2t-03-1987"
+        Assert.assertEquals(expected, actual)
+
+        actual = employee(birthDay = "21-03-558y").copyWithCorrectBirthday().birthday
+        expected = "21-03-558y"
+        Assert.assertEquals(expected, actual)
+    }
+
+    private fun employee(birthDay: String) = Employee(
+        id = "",
+        firstName = "",
+        lastName = "",
+        birthday = birthDay,
+        avatarUrl = "",
+        specialties = emptyList()
+    )
+}
